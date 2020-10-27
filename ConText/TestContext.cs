bb453d51c38219a208694e7afba41858f3e579d9
anjiaohai <944979868@qq.com>
2020-10-27 21:44:48 +0800

    20201027测试

---
+++ ConText/TestContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace helloworldAPI.ConText
+{
+    public class TestContext:DbContext
+    {
+        public TestContext(DbContextOptions<TestContext> options):base(options)
+        {
+
+
+        }
+        public DbSet<Student> Student{get;set}
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,9 @@
+namespace helloworldAPI.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public int age { get; set; }
+        public string Name { get; set; }
+    }
+}
