91d4b366c6d920d3d3d1bbfd2a335e6f788ebd23
anjiaohai <944979868@qq.com>
2020-10-27 22:07:56 +0800

    测试

--- Model/Student.cs
+++ Model/Student.cs
@@ -2,7 +2,6 @@ namespace helloworldAPI.Model
 {
     public class Student
     {
-        public int ID { get; set; }
         public int age { get; set; }
         public string Name { get; set; }
     }
